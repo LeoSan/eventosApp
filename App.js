85358a3760c89dd5590bc3045a1735cb314ad214
LeoSan <cuenca623@gmail.com>
2021-03-04 01:26:59 -0600

    Autor: Leonard Cuenca, Descripción: Desarrollo final de Layout usando Flex solo falta la documentaciòn del proyecto

--- App.js
+++ App.js
@@ -1,19 +1,104 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
 
   return (
     <>
-    <View>
+    <ScrollView>
         <View style={{ flexDirection:'row' }}>
             <Image
                 style={styles.banner}
                 source={ require('./assets/img/bg.jpg')}
             />
         </View>
-    </View>
+        <View style={styles.contenedor}>
+            <Text style={styles.titulo}>Qué hacer en Paris</Text>
+        </View>
+        <ScrollView
+          horizontal
+        >
+            <View style={{ flexDirection:'row' }}>
+                <Image
+                    style={styles.ciudad}
+                    source={ require('./assets/img/actividad1.jpg')}
+                />
+            </View>
+            <View style={{ flexDirection:'row' }}>
+                <Image
+                    style={styles.ciudad}
+                    source={ require('./assets/img/actividad2.jpg')}
+                />
+            </View>
+            <View style={{ flexDirection:'row' }}>
+                <Image
+                    style={styles.ciudad}
+                    source={ require('./assets/img/actividad3.jpg')}
+                />
+            </View>
+            <View style={{ flexDirection:'row' }}>
+                <Image
+                    style={styles.ciudad}
+                    source={ require('./assets/img/actividad4.jpg')}
+                />
+            </View>
+            <View style={{ flexDirection:'row' }}>
+                <Image
+                    style={styles.ciudad}
+                    source={ require('./assets/img/actividad5.jpg')}
+                />
+            </View>
+        </ScrollView>
+            <Text style={styles.titulo}>Mejores lugares</Text>
+            <View style={{ flexDirection:'row' }}>
+                <Image
+                    style={styles.mejores}
+                    source={ require('./assets/img/mejores1.jpg')}
+                />
+            </View>
+            <View style={{ flexDirection:'row' }}>
+                <Image
+                    style={styles.mejores}
+                    source={ require('./assets/img/mejores2.jpg')}
+                />
+            </View>
+            <View style={{ flexDirection:'row' }}>
+                <Image
+                    style={styles.mejores}
+                    source={ require('./assets/img/mejores3.jpg')}
+                />
+            </View>  
+
+            <Text style={styles.titulo}>Vivir en los Angeles</Text>
+            
+            <View style={styles.listado}>
+                <View style={styles.listadoItem}>
+                    <Image
+                        style={styles.mejores}
+                        source={ require('./assets/img/hospedaje1.jpg')}
+                    />
+                </View>  
+                <View style={styles.listadoItem}>
+                    <Image
+                        style={styles.mejores}
+                        source={ require('./assets/img/hospedaje2.jpg')}
+                    />
+                </View>
+                <View style={styles.listadoItem}>
+                    <Image
+                        style={styles.mejores}
+                        source={ require('./assets/img/hospedaje3.jpg')}
+                    />
+                </View>  
+                <View style={styles.listadoItem}>
+                    <Image
+                        style={styles.mejores}
+                        source={ require('./assets/img/hospedaje3.jpg')}
+                    />
+                </View>                         
+              </View>
+    </ScrollView>
     </>
   );
 }
@@ -23,4 +108,32 @@ const styles = StyleSheet.create({
     height:250,
     flex:1,
   },
+  titulo:{
+    fontWeight:'bold',
+    fontSize:24,
+    marginVertical:20,
+  },
+  contenedor:{
+    marginHorizontal:10, 
+  },
+  ciudad:{
+      width:200,
+      height:200,
+      marginRight:10
+  },
+  mejores:{
+    width:'100%',
+    height:200,
+    marginVertical:5
+  },
+  listado:{
+    flexDirection:'row',
+    flexWrap:'wrap',
+    justifyContent:'space-between',
+    
+
+  },
+  listadoItem:{
+    flexBasis:'49%',
+  }
 });
